c3f92c44632c8c8403faed75330edb60462f9c3e
TrubitsynEgor <trubitsynwork74@gmail.com>
2023-06-08 18:53:40 +0500

    Job test

---
+++ src/js/_vendor.js
@@ -0,0 +1,6 @@
+import "./vendor/focus-visible.js";
+// import "./vendor/uikit.min";
+// import "./vendor/uikit-icons.min";
+import "uikit/dist/js/uikit.js";
+import "uikit/dist/js/uikit-core.js";
+import "uikit/dist/js/uikit-icons.js";
---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+"use-strict";
+import "./_vendor";
+import vars from "./_vars";
+import "./_functions";
+import "./_components";
+import UIkit from "uikit";
+
+//* Accordion with JS only, it's work, but styles not connected
+// UIkit.accordion("#accordion", {
+//   active: 0,
+//   toggle: "#title",
+//   content: "#target",
+// });
+
+UIkit.icon("#testEl", {
+  icon: "twitter",
+});
+
+// UIkit.modal.alert("UIkit alert!");
+// UIkit.modal.confirm("UIkit confirm!");
+// UIkit.modal.prompt("Name:", "Your name");
+// UIkit.modal.dialog("<p>UIkit dialog!</p>");
